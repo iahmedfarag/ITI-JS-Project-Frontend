1e6935682a78190495d6ef454acdca4b0186f501
ahmed farag <ahmedfarag1k@gmail.com>
2024-12-31 10:40:04 +0200

    products/single products

--- pages/products/script.js
+++ pages/products/script.js
@@ -192,10 +192,7 @@ function filterProductsByCategory(categoryId, products) {
                     <p class="">${product.category.name}</p>
                     <p class="price">${product.price}$</p>
                 </div>
-                <div class="hover-content">
-                    <p style="text-align: center; margin-bottom: 20px">
-                        ${product.description}
-                    </p>
+                <div class="">
                     <div class="hover-buttons">
                         <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960"
                             width="35px" fill="#b0b0b0">
@@ -242,7 +239,7 @@ document.getElementById("cars").addEventListener("change", function () {
     let sortedCartona = "";
     sortedProducts.forEach((product, index) => {
         sortedCartona += `<div class="col-md-4">
-                    <div class="card" onclick="location.href='singleProduct.html?id=${product._id}'" >
+                    <div class="card" onclick="location.href='../singleProducts/singleProduct.html?id=${product._id}'" >
                         <img id="img-${index}" class="card-img-top" src="${product.mainImage}" alt="Card image" />
                         <div class="card-body">
                             <p class="text-center my-1">${product.name}</p>
@@ -273,85 +270,3 @@ document.getElementById("cars").addEventListener("change", function () {
 
     document.getElementById("myData").innerHTML = sortedCartona;
 });
-
-// function renderProducts() {
-//   const startIndex = (currentPage - 1) * productsPerPage;
-//   const endIndex = startIndex + productsPerPage;
-//   const productsToDisplay = products.slice(startIndex, endIndex);
-//   let cartona = "";
-//   productsToDisplay.forEach((product, index) => {
-//     cartona += `<div class="col-md-4">
-//                     <div class="card" onclick="location.href='singleProduct.html?id=${product._id}'" >
-//                         <img id="img-${index}" class="card-img-top" src="${product.mainImage}" alt="Card image" />
-//                         <div class="card-body">
-//                             <p class="text-center my-1">${product.name}</p>
-//                             <p class="cat text-center">${product.category.name}</p>
-//                             <p class="price text-center">${product.price}$</p>
-//                         </div>
-//                         <div class="hover-content">
-//                             <p style="text-align: center; margin-bottom: 20px">
-//                                 ${product.description}
-//                             </p>
-//                             <div class="hover-buttons">
-//                 <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960"
-//                     width="35px" fill="#b0b0b0">
-//                     <path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z" />
-//                 </svg>
-//                 <button style="border: 0;background-color: #74a32f;color: white;width: 40%;height: 40px;border-radius: 8px;">
-//                     Add To Card
-//                 </button>
-//                 <svg xmlns="http://www.w3.org/2000/svg" height="35px" viewBox="0 -960 960 960"
-//                     width="35px" fill="#b0b0b0">
-//                     <path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" />
-//                 </svg>
-//             </div>
-//                         </div>
-//                     </div>
-//                 </div>`;
-//   });
-//   document.getElementById("myData").innerHTML = cartona;
-// }
-// function setupPagination() {
-
-//   const pageCount = Math.ceil(products.length / productsPerPage);
-//   const paginationContainer = document.getElementById("pagination");
-//   paginationContainer.innerHTML = "";
-//   for (let i = 1; i <= pageCount; i++) {
-//     const pageItem = document.createElement("div");
-//     pageItem.classList.add("paginationItem");
-//     pageItem.innerHTML = `<p>${i}</p>`;
-//     if (i === currentPage) {
-//       pageItem.classList.add("active");
-//     }
-//     pageItem.addEventListener("click", () => {
-//       currentPage = i;
-//       renderProducts();
-//       setupPagination();
-//     });
-//        paginationContainer.appendChild(pageItem);
-
-//   }
-// }
-// var productsPerPage;
-// if (lengthOfFilteredResponseByCategory>0) {
-// productsPerPage = lengthOfFilteredResponseByCategory;
-// }else if(lengthOfFilteredResponseByPrice>0){
-// productsPerPage=lengthOfFilteredResponseByPrice;
-// }else{
-//   productsPerPage = 12;
-// }
-// let currentPage = 1;
-// let products = [];
-// function fetchProducts() {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("GET", `https://iti-js-project-backend.vercel.app/api/products/`);
-//   xhr.send();
-//   xhr.addEventListener("readystatechange", function () {
-//     if (xhr.readyState === 4 && xhr.status === 200) {
-//       products = JSON.parse(xhr.response);
-//       renderProducts();
-//       setupPagination();
-//     }
-//   });
-// }
-// fetchProducts();
